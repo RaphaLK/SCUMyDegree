a70afa3684fc7c46f1612b64a048306dac99d34e
joshsj89 <joshsj89@gmail.com>
2024-03-29 12:10:03 -0700

    Fix href warnings

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,15 +8,15 @@ function NavBar() {
                 <Link to="/">
                     <img src="/external/primaryi322-c8z6.svg" className={styles['images']} alt="SCUMyDegree Logo" />
                 </Link>
-                <a href="">About</a>
-                <a href="">Donate</a>
-                <a href="">FAQ</a>
+                <a href="/">About</a>
+                <a href="/">Donate</a>
+                <a href="/">FAQ</a>
             </div>
             <div className={styles['right-items']}>
-                <a href="" >
+                <a href="/" >
                     <img src="/external/cogregular361i322-4zte-200w.png" className={styles['images']} alt="Settings" />
                 </a>
-                <a href="">
+                <a href="/">
                     <img src="/external/primaryi322-dezm.svg" className={styles['images']} alt="My Account" />
                 </a>
             </div>
